bbf21cb2aaa6885a88ceb82397c6708f35f10450
dusicab <beleslijindusica@gmail.com>
2017-06-30 16:24:47 +0200

    Add files via upload

---
+++ niske.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -
+
+rečenica =  "Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+rečenica = rečenica.lower ()
+lista_reči = rečenica.split(" ")
+print(lista_reči)
+
+niska = "Dokle ću više biti niska!"
+karakteri = list(niska)
+print(karakteri)
