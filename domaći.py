0976031e32da503bc21fe8b157c97bc1912f22e3
dusicab <beleslijindusica@gmail.com>
2017-06-30 22:57:13 +0200

    Add files via upload

---
+++ domaći.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -
+recenica = "Mislim da je seminar lingvistike kul."
+sintagma_1 = recenica[13:32]
+print(sintagma_1)
+lista_reči = recenica.split ("Mislim da je seminar lingvistike kul.")
+print(lista_reči)
