1f6fdd46b97f2e2f0c3695392011bcbc0b6385e9
dusicab <beleslijindusica@gmail.com>
2017-07-01 17:16:13 +0200

    Add files via upload

---
+++ rečnici.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+telefonski_imenik = {"Paja Patak": 123456, "Mini Maus": 234567, "Šilja" : 345678}
+vukajlija = {"sojanica": "Posna pljeskavica. Garantovano bez trihinele.", "jahanje": "Omiljena aktivnost šefova za koju je potrebno da radnik ima konjske živce.", "šef": "Čovek koji nema smisao za umor."}
+vrste_reči = {"imenice": ["polaznik", "seminar", "lingvistika", "Isidora"], "glagoli": ["slušati", "crtati", "jesti"], "zamenice": ["on", "ona", "ono"]}
+
+print(telefonski_imenik["Mini Maus"])
+print(vukajlija["šef"])
+print(vrste_reči["imenice"])
+print(telefonski_imenik)
+vukajlija["šef"] = "Miloš"
+print(vukajlija)
+print(telefonski_imenik.keys())
+print(telefonski_imenik.values())
